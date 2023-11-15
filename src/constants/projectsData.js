1974d0467924e00c1f8450f4959b21ecd5eadcde
marshalljordan1 <jordan_marshall@live.co.uk>
2023-11-15 12:20:52 +0100

    add Next.js project

--- src/constants/projectsData.js
+++ src/constants/projectsData.js
@@ -1,6 +1,7 @@
 import JapaneseToursim from "../assets/img/JapaneseToursimWebsite.png";
 import Bookmark from "../assets/img/Bookmark.png";
 import github from "../assets/img/github.png";
+import nextjs from "../assets/img/nextjs.png";
 
 export const navLinks = [
   {
@@ -23,20 +24,26 @@ export const navLinks = [
 export const projects = [
   {
     id: 1,
-    name: "Tourism Website",
+    name: "HTML + CSS",
     image: JapaneseToursim,
     source_code_link: "https://sprint1-html-and-css.vercel.app/",
   },
   {
     id: 2,
-    name: "Bookmark Website",
+    name: "HTML + Bootstrap + Sass",
     image: Bookmark,
     source_code_link: "https://sprint2-bootstrap-and-sass.vercel.app/",
   },
   {
-    id: 2,
-    name: "Github Repositories Project",
+    id: 3,
+    name: "React.js + Typescript",
     image: github,
     source_code_link: "https://git-repo-search-kohl.vercel.app/",
+  },
+  {
+    id: 4,
+    name: "Next.js",
+    image: nextjs,
+    source_code_link: "https://next-js-alpha-three.vercel.app/",
   },
 ];
